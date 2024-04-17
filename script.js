a9436cdaa996e438b386aba723ad9586da318ae1
Priyadarshani panigrahy <117714258+priyadarshanipanigrahy28@users.noreply.github.com>
2024-04-17 21:12:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+var msg1 = document.getElementById("message1");
+var msg2 = document.getElementById("message2");
+var msg3 = document.getElementById("message3");
+
+var answer = Math.floor(Math.random()*100) + 1;
+var no_of_guesses = 0;
+var guessed_nums = [];
+
+function play(){
+  var user_guess = document.getElementById("guess").value;
+  if(user_guess < 1 || user_guess > 100){
+    alert("Please enter a valid number between 1 and 100");
+  }
+  else if(user_guess > 1 || user_guess < 100){
+    guessed_nums.push(user_guess);
+    no_of_guesses+= 1;
+
+    if(user_guess < answer){
+      msg1.textContent = "Your guess is too low.";
+      msg2.textContent = "No. of guesses: " + no_of_guesses;
+      msg3.textContent = "Guessed Numbers are: " + guessed_nums;
+    }
+    else if(user_guess > answer){
+      msg1.textContent = "Your guess is too high.";
+      msg2.textContent = "No. of guesses: " + no_of_guesses;
+      msg3.textContent = "Guessed Numbers are: " + guessed_nums;
+    }
+    else if(user_guess == answer){
+      msg1.textContent = "Yippie You Win!!";
+      msg2.textContent = "The Number Was: " + answer;
+      msg3.textContent = "You guessed it in:" + no_of_guesses + "guesses";
+      document.getElementById("my_btn").disabled = true;
+     }
+      
+  }
+}
